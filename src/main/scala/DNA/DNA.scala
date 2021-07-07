18d9be8614db194f039d0bdc62e8089e0d203528
molvlad <molvlad@frtk.ru>
2021-07-07 18:46:31 +0200

    some auxiliary classes for smith waterman algorithm

---
+++ src/main/scala/DNA/DNA.scala
@@ -0,0 +1,81 @@
+package DNA
+
+import chisel3._
+import chisel3.experimental.ChiselEnum
+
+object Scores {
+  val width = 32
+  val matchScore = 1.S(2.W)
+  val mismatchScore = -1.S(2.W)
+  val gapScore = 2.S(3.W)
+  val lowerBound = 0.S
+  val useLowerBound = true
+}
+
+object Elements extends ChiselEnum {
+  val A, G, C, T = Value
+}
+
+class substituteElements(width: Int = Scores.width) extends Module {
+  val io = IO(new Bundle {
+    val first = Input(Elements())
+    val second = Input(Elements())
+    val out = Output(SInt(width.W))
+  })
+
+  io.out := Scores.matchScore
+  when (io.first === io.second) {
+    io.out := Scores.matchScore
+  } .otherwise {
+    io.out := Scores.mismatchScore
+  }
+}
+
+// it's necessary to check it with simulation
+class calculateCell(width: Int = Scores.width) extends Module {
+  val io = IO(new Bundle {
+    val upper = Input(SInt(width.W))
+    val left = Input(SInt(width.W))
+    val diagonal = Input(SInt(width.W))
+    val first = Input(Elements())
+    val second = Input(Elements())
+
+    val out = Output(SInt(width.W))
+  })
+
+  val substituteElements = Module(new substituteElements)
+  substituteElements.io.first := io.first
+  substituteElements.io.second := io.second
+
+  val upper, diag, left, diagUpper, diagLeft, max = Wire(SInt(width.W))
+  diag := io.diagonal + substituteElements.io.out
+  upper := io.upper - Scores.gapScore
+  left := io.left - Scores.gapScore
+
+  when (diag > upper) {
+    diagUpper := diag
+  } .otherwise {
+    diagUpper := upper
+  }
+  when (diag > left) {
+    diagLeft := diag
+  } .otherwise {
+    diagLeft := left
+  }
+  when (diagLeft > diagUpper) {
+    max := diagLeft
+  } .otherwise {
+    max := diagUpper
+  }
+
+  if (Scores.useLowerBound) {
+    io.out := max
+  } else {
+    when (max > Scores.lowerBound) {
+      io.out := max
+    } .otherwise {
+      io.out := Scores.lowerBound
+    }
+  }
+}
+
---
+++ src/main/scala/elastic/Waterman.scala
@@ -0,0 +1,22 @@
+package elastic
+
+import chisel3._
+import chisel3.util._
+import DNA._
+
+class Waterman extends Module {
+  val io = IO(new Bundle {
+    val out = new DecoupledIO(SInt(Scores.width.W))
+    val x = Flipped(new DecoupledIO(Elements()))
+    val y = Flipped(new DecoupledIO(Elements()))
+  })
+
+  val s = Module(new substituteElements)
+  s.io.first := io.x.bits
+  s.io.second := io.y.bits
+  io.out.bits := s.io.out
+
+  io.x.ready := true.B
+  io.out.valid := true.B
+  io.y.ready := true.B
+}
